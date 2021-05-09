ec9fc821258d9fa4a22303df5e89f549739199c9
Roger Ferro <rogerferro@gmail.com>
2021-05-09 02:05:29 -0300

    Static charts

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/rogerferro" target="_blank" rel="noreferrer">Roger Ferro</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Contato: <a href="https://instagram.com/rogerferro" target="_blank" rel="noreferrer">rogerferro@gmail.com</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
