bbad7372251ca786cc0d48f9996e019b8befa15c
Roger Ferro <rogerferro@gmail.com>
2021-05-09 02:46:34 -0300

    Alteracao contato

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/rogerferro" target="_blank" rel="noreferrer">Roger Ferro</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Contato: <a href="https://instagram.com/rogerferro" target="_blank" rel="noreferrer">rogerferro@gmail.com</a></small></p>
+      Contato: <a href="https://instagram.com/roger.ferro" target="_blank" rel="noreferrer">rogerferro@gmail.com</a></small></p>
             </div>
         </footer>
     );
